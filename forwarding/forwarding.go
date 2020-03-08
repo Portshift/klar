88041c60c0ff25977ea01f4ce1b050e2b32ab7e7
rafi <rafi@portshift.io>
2020-03-08 15:44:39 +0200

    polishing

--- forwarding/forwarding.go
+++ forwarding/forwarding.go
@@ -4,7 +4,7 @@ import (
 	"github.com/Portshift-Admin/klar/clair"
 )
 
-type ContextualVulnerabilities struct {
+type ImageVulnerabilities struct {
 	Vulnerabilities []*clair.Vulnerability `json:"vulnerability"`
 	Image           string                 `json:"image,omitempty"`
 }
